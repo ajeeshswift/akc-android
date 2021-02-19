2b962034f34773835e8b5ea8ae3d9e988a11fc84
Thanu Ganesh <thanunr5@gmail.com>
2021-02-19 14:53:39 +0530

    Rx2 added

---
+++ app/src/main/java/com/swift/akc/AkcApplication.java
@@ -0,0 +1,13 @@
+package com.swift.akc;
+
+import android.app.Application;
+
+import com.androidnetworking.AndroidNetworking;
+
+public class AkcApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AndroidNetworking.initialize(getApplicationContext());
+    }
+}
--- app/src/main/java/com/swift/akc/LoginActivity.java
+++ app/src/main/java/com/swift/akc/LoginActivity.java
@@ -4,8 +4,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONArrayRequestListener;
+import com.rx2androidnetworking.Rx2AndroidNetworking;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.function.Function;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
 
 public class LoginActivity extends BaseAppCompactActivity {
 
@@ -25,8 +39,32 @@ public class LoginActivity extends BaseAppCompactActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                loginApiCall();
             }
         });
+    }
+
+    public void loginApiCall(){
+        AndroidNetworking.get("https://fierce-cove-29863.herokuapp.com/getAllUsers/{pageNumber}")
+                .addPathParameter("pageNumber", "0")
+                .addQueryParameter("limit", "3")
+                .addHeaders("token", "1234")
+                .setTag("test")
+                .setPriority(Priority.HIGH)
+                .build()
+                .getAsJSONArray(new JSONArrayRequestListener() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        // do anything with response
+                        Toast.makeText(getApplicationContext(),"success",Toast.LENGTH_SHORT).show();
+                    }
+
+
+                    @Override
+                    public void onError(ANError error) {
+                        // handle error
+                        Toast.makeText(getApplicationContext(),"failure",Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
